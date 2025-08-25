d3c9e9fa871d645f0b74c35c79c0e10d74ab845b
David Lad <david.ladapo@detectortesters.com>
2025-08-25 16:10:04 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,3 @@
-
 (defproject hyperion "0.1.0-SNAPSHOT"
-  :description "Hyperion Instance"
-  :url "https://github.com/Hose-Monster/hyperion"
-  :license {:name "MIT License"
-            :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
-  :main ^:skip-aot hyperion.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+  :description "Hyperion Project"
+  :dependencies [[org.clojure/clojure "1.10.3"]])
